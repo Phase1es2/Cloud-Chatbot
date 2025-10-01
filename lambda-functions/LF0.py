bd82aa737cced8d256bfc2fa8a6376598c7990cf
Phase1es2 <hyang17@uw.edu>
2025-10-01 17:38:07 -0400

    temp LF-0 and LF1

--- lambda-functions/LF0.py
+++ lambda-functions/LF0.py
@@ -1,7 +1,32 @@
-import json 
+import json
 
 def lambda_handler(event, context):
+    body = event.get("body")
+    if body:
+        body = json.loads(body)
+    else:
+        body = event
+
+    print("Messages:", body.get("messages"))
+
+    resp = {
+        "messages": [
+            {
+                "type": "string",
+                "unstructured": {
+                    "id": "string",
+                    "text": "Hello from Lambda!",
+                    "timestamp": "string"
+                }
+            }
+        ]
+    }
+
     return {
-        'statusCode': 200,
-        'body': json.dumps("I'm still under development. Please come back later.")
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(resp)
     }
